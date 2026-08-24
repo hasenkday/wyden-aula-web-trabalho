ff5ca387408b6354a8f8fc212f5373b13ab37055
hasenkday <nadia.hasenkday@gmail.com>
2026-08-24 19:59:08 -0300

    testing

---
+++ components/organisms/header-nav/header-nav.js
@@ -0,0 +1,17 @@
+function HeaderNav() {
+  return `
+    <div>
+        <header>
+            <h1>🚀 Portal de Desenvolvimento Web</h1>
+            <p>UniMetrocamp Wyden - Turma de HTML5 & CSS3</p>
+        </header>
+
+        <nav>
+            <a href="index.html">Home</a>
+            <a href="pagina0.html">Mini Currículo</a>
+            <a href="pagina1.html">Hospedagem</a>
+            <a href="pagina2.html">Galeria de Projetos</a>
+        </nav>
+    </div>
+  `;
+}
