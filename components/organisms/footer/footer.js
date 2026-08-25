bbdec77db6afbe08ad39df6909a35d22ec2917c1
Nadia Hase <nadia.hasenkday@gmail.com>
2026-08-24 22:44:26 -0300

    refactor: create footer component

---
+++ components/organisms/footer/footer.js
@@ -0,0 +1,10 @@
+function Footer() {
+  return `
+    <footer>
+      <p>
+        &copy; 2026 UniMetrocamp Wyden - Prof. Leonardo Ramos de Oliveira. Todos os direitos
+        reservados.
+      </p>
+    </footer>
+  `;
+}
