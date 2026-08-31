6b0fc95b133d4c85a5a65277c02ba5a37d6d5798
Nadia Hase <nadia.hasenkday@gmail.com>
2026-08-31 10:26:16 -0300

    fix: Exemplo10 and some other example styles

--- projects/aula-4/exemplo4.js
+++ projects/aula-4/exemplo4.js
@@ -7,7 +7,7 @@ function Exemplo4() {
             .caixa {
                 width: 100%;
                 height: 350px;
-                background-image: url('https://img.freepik.com/vetores-premium/nave-espacialfoguete-em-tecnologia-cientifica-uma-ilustracao-de-icone-vetorial-de-desenho-animadoplano-isolado_950157-4191.jpg');
+                background-image: url('https://images.unsplash.com/vector-1774701191570-5a035338e1fe?q=80&w=1057&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
                 border: 2px solid black;
                 background-size: cover;
                 background-repeat: no-repeat;
